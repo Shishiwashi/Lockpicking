5f46fcf51778c22124464c39d9085ff955483c4f
Shishiwashi <shishiwashi@hotmail.com>
2014-02-25 21:37:50 -0300

    Polymerization

---
+++ LockPick Project/Assets/Scripts/LevelsManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelsManager : MonoBehaviour
+{
+	public WorldProperties[] worlds;
+	public GameObject worldPrefab;
+	public int startingWorld;
+	public UIScrollList levelsScroll;
+	public UIScrollList worldsScroll;
+	
+	void Start ()
+	{
+		foreach(WorldProperties p in worlds)
+		{
+			worldsScroll.CreateItem(worldPrefab);
+			World newWorld = worldsScroll.GetItem(worldsScroll.Count-1).gameObject.GetComponent<World>();
+			newWorld.properties = p;
+			newWorld.manager = this;
+		}
+	}
+	
+	public void BringLevelPanel()
+	{
+		levelsScroll.transform.parent.GetComponent<UIInteractivePanel>().BringIn();
+		worldsScroll.transform.parent.GetComponent<UIInteractivePanel>().Dismiss();
+	}
+	
+	public void BringWorldPanel()
+	{
+		levelsScroll.transform.parent.GetComponent<UIInteractivePanel>().Dismiss();
+		worldsScroll.transform.parent.GetComponent<UIInteractivePanel>().BringIn();
+		levelsScroll.ClearList(true);
+	}
+}
